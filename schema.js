6acafee2187e4df81a55f7f93629e5fb80013926
baesparza <bruno.be81@gmail.com>
2017-08-22 11:15:56 -0500

    feat(data): simulate db

--- schema.js
+++ schema.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const {
     GraphQLObjectType,
     GraphQLString,
@@ -8,12 +9,14 @@ const {
     GraphQLNonNull
 } = require('graphql');
 
+/* 
 // Hard coded data
 const customers = [
     { id: '1', name: 'Bruno Esparza', email: 'bruno.be81@gmail.com', age: 19 },
     { id: '2', name: 'Jonathan Rocero', email: 'hippie@gmail.com', age: 21 },
     { id: '3', name: 'Emmanuel Auquilla', email: 'emma@email.com', age: 18 }
-];
+]; 
+*/
 
 // root query
 const CustomerType = new GraphQLObjectType({
@@ -36,17 +39,21 @@ const RootQuery = new GraphQLObjectType({
                 id: { type: GraphQLString }
             },
             resolve(parentValue, args) {
-                for (let i = 0; i < customers.length; i++) {
+                /* for (let i = 0; i < customers.length; i++) {
                     if (customers[i].id == args.id) {
                         return customers[i];
                     }
-                }
+                } */
+                return axios.get(`http://localhost:3000/customers/${args.id}`)
+                    .then(res => res.data);
             }
         },
         customers: {
             type: new GraphQLList(CustomerType),
             resolve(parentValue, args) {
-                return customers;
+                /* return customers; */
+                return axios.get(`http://localhost:3000/customers`)
+                    .then(res => res.data);
             }
         }
     }
